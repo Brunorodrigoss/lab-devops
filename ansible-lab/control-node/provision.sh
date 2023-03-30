0b0adc1e967d9ba8c5a3b0d792079b5c78ab849d
Bruno Rodrigo <brunorodrigo@Brunos-MacBook-Pro-2.local>
2023-03-30 17:49:51 +0100

    Create control-node machine

---
+++ ansible-lab/control-node/provision.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo yum -y install epel-release
+echo "Installing ansible"
+sudo yum -y install ansible
+cat <<EOT >> /etc/hosts
+192.168.2.2 control-node
+192.168.2.3 app01
+192.168.2.4 db01
+EOT
