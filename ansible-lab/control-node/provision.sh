36a63a92e690ca2c4e6aa5f5740ebf58cab9e85b
Bruno Rodrigo <bnorodrigo@gmail.com>
2023-04-05 11:09:55 +0100

    Config playbooks

--- ansible-lab/control-node/provision.sh
+++ ansible-lab/control-node/provision.sh
@@ -3,17 +3,10 @@
 sudo yum -y install epel-release
 echo "Installing ansible"
 sudo yum -y install ansible
+
+echo "Setup machines at /etc/hosts"
 cat <<EOT >> /etc/hosts
 192.168.2.2 control-node
 192.168.2.3 app01
 192.168.2.4 db01
 EOT
-
-sudo -u vagrant ssh-keygen -t rsa -N '' <<<''
-
-if [ -e public_key.txt ];
-then
-    rm public_key.txt
-fi
-
-cat /home/vagrant/.ssh/id_rsa.pub > /vagrant/public_key.txt
